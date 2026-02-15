8dacb43006aafe14d0ffa89cc5b9987e06759cc3
DanNessler <dan.nessler@gmail.com>
2026-02-15 16:42:58 +0100

    Create test.js

---
+++ scripts/test.js
@@ -0,0 +1,9 @@
+console.log('External script loaded successfully');
+```
+- Scroll down, click "Commit changes"
+
+**3. Get your CDN URL**
+
+Your URL format will be:
+```
+https://cdn.jsdelivr.net/gh/[DanNessler]/sooon-scripts@main/scripts/test.js
